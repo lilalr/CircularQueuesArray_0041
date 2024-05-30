3233a6006ad0ce9614752b12e3d775465d2af9da
lilalr <gurennut@gmail.com>
2024-05-30 18:25:12 +0700

    cek apakah antrian kosong

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -64,5 +64,11 @@ public:
 		int FRONT_position = FRONT;
 		int REAR_Position = REAR;
 
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "\nElements in the queue are...\n"
 	}
 };
